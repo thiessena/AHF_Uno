e368aaf9d538b1f4e1e9ee45ff56acc97665697a
Viktor <150668392+ViktorKon@users.noreply.github.com>
2023-12-04 11:24:37 +0100

    Update Handkarten.java

--- Handkarten.java
+++ Handkarten.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class UNOHandkartenAnzeige {
+public class HandkartenAnzeige {
     public static void main(String[] args) {
         ArrayList<String> handkarten = generateUNOHandkarten();
         
