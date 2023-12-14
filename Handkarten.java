10dd484ba58514cc170581e9652f29328c9b4a46
Viktor <150668392+ViktorKon@users.noreply.github.com>
2023-12-14 09:27:38 +0100

    Handkarten.java

--- Handkarten.java
+++ Handkarten.java
@@ -1,42 +1,19 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class HandkartenAnzeige {
+public class Handkarten {
     public static void main(String[] args) {
-        ArrayList<String> handkarten = generateUNOHandkarten();
-        
-        System.out.println("Deine UNO Handkarten:");
-        for (String karte : handkarten) {
-            System.out.println(karte);
+        ArrayList<String> handkarten = generateHandkarten();
         }
+    public void sortieren(){
+        
     }
+    public Karte ziehen(int n){
     
-    public static ArrayList<String> generateUNOHandkarten() {
-        ArrayList<String> handkarten = new ArrayList<>();
+    }
+    public void legen(){
         
-        // Farben
-        ArrayList<String> farben = new ArrayList<>(Arrays.asList("Rot", "Blau", "Grün", "Gelb"));
-        
-        // Zahlenkarten
-        for (String farbe : farben) {
-            for (int zahl = 0; zahl <= 9; zahl++) {
-                handkarten.add(farbe + " " + zahl);
-            }
-        }
-        
-        // Aktionskarten
-        for (String farbe : farben) {
-            handkarten.add(farbe + " Aussetzen");
-            handkarten.add(farbe + " Ziehe Zwei");
-            handkarten.add(farbe + " Richtungswechsel");
-        }
-        
-        // Farbe wählen Karten
-        for (int i = 0; i < 4; i++) {
-            handkarten.add("Farbe wählen");
-            handkarten.add("Farbe wählen + Vier ziehen");
-        }
-        
+    }
         return handkarten;
     }
 }
