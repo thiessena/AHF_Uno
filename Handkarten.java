b2e410565f5eec9e9b09262cffb28b24a883496e
thiessena <a.thiessen@ahfs-detmold.de>
2024-01-18 08:52:13 +0100

    Handkarte geändert

--- Handkarten.java
+++ Handkarten.java
@@ -1,18 +1,57 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
+/**
+ * Verwaltet die Handkarten
+ */
 public class Handkarten {
+    ArrayList<Karte> karten = new ArrayList<Karte>();
+    ArrayList<Karte> ausgewaehlteKarten = new ArrayList<Karte>();
 
     public void sortieren() {
 
     }
 
-    public Karte ziehen(int n) {
-
-        return new Karte("", 1);
+    /**
+     * Gibt die Karte zurück, die an der Stelle index liegt. Die Karte wird von den
+     * Handkarten entfernt.
+     * 
+     * @param index
+     * @return
+     */
+    public Karte ziehen(int index) {
+        Karte k = karten.get(index);
+        karten.remove(index);
+        return k;
     }
 
-    public void legen() {
-
+    public void karteAuswaehlen(int index) {
+        if (!ausgewaehlteKarten.contains(karten.get(index))) {
+            ausgewaehlteKarten.add(karten.get(index));
+        }
     }
+
+    public void karteAbwaehlen(int index) {
+        if (!ausgewaehlteKarten.contains(karten.get(index))) {
+            ausgewaehlteKarten.remove(karten.get(index));
+        }
+    }
+
+    /**
+     * Wählt die Karten aus, die
+     * 
+     * @param karten
+     */
+    public void kartenAuswaehlen(ArrayList<Karte> karten) {
+        ausgewaehlteKarten.removeAll(karten);
+        ausgewaehlteKarten.addAll(karten);
+    }
+
+    public void alleKartenAbwaehlen() {
+        ausgewaehlteKarten = new ArrayList<>();
+    }
+
+    public ArrayList<Karte> getAusgewaehlteKarten() {
+        return ausgewaehlteKarten;
+    }
+
 }
