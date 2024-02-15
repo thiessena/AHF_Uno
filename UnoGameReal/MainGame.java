6914a16e80ad05ff9b80d8377dfd2083f06ae076
Tim Tabert <74070462+Tireis@users.noreply.github.com>
2024-02-15 09:28:25 +0100

    Create MainGame.java

---
+++ UnoGameReal/MainGame.java
@@ -0,0 +1,4 @@
+// hier sollten die allgemeinden Spieloperatoren angegeben werden
+// der "Ablagestapel" soll lediglich eine Karte sein die oben angezeigt und gegetted werden muss
+// Der "Ziehstapel" soll eine Liste sein die vorher random preperiert wurde, hier wird die oberste Karte gezogen wenn der Spieler sie zieht
+// weitere Regeln
