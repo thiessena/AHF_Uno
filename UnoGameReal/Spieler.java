143f4e879b1e81f27959f76787165493f5158588
TobiSiem <105721779+TobiSiem@users.noreply.github.com>
2024-02-26 11:17:04 +0100

    Spieler.java

--- UnoGameReal/Spieler.java
+++ UnoGameReal/Spieler.java
@@ -1,1 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class Spieler {
+   
+    private String name;
+   
+    private List<Karte> handKarten;
+   
+    public Spieler(String name) {
+       this.name = name;
+       this.handKarten = new ArrayList<>();
+   }
+   
+   public String getName() {
+       return name;
+   }
+   
+   public List<Karte> getHandKarten() {
+       return handKarte;
+   }
+   
+   public void addKarte(Karte karte) {
+       handKarten.add(karte);
+   }
+   
+   public void removeKarte(Karte karte) {
+       handkarten.remove(karte);
+   }
+
+}
