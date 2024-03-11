9553ff2fa44fc2120556996c3c049cff7d89b22c
tryTimCatch <150668042+tryTimCatch@users.noreply.github.com>
2024-03-11 11:17:35 +0100

    Update Spieler.java

--- UnoGameReal/Spieler.java
+++ UnoGameReal/Spieler.java
@@ -6,10 +6,12 @@ public class Spieler {
     private String name;
     public Karte abgelegteKarte;
     private List<Karte> handKarten;
+    private List<Karte> handKartenS2;
    
     public Spieler(String name) {
        this.name = name;
        this.handKarten = new ArrayList<>();
+       this.handKartenS2 = new ArrayList<>();
    }
    
    public String getName() {
@@ -19,21 +21,33 @@ public class Spieler {
    public List<Karte> getHandKarten() {
        return handKarten;
    }
+
+    public List<Karte> getHandKartenS2() {
+        return handKartenS2;
+    }
    
    public void addKarte(Karte karte) {
        handKarten.add(karte);
    }
    
+    public void addKarteS2(Karte karte) {
+        handKartenS2.add(karte);
+    }
+   
    public void removeKarte(Karte karte) {
        handKarten.remove(karte);
    }
 
-   public void handkarteHinzufügen(Karte getKarte){
-        handKarten.add(getKarte);
-   }
+    public void removeKarteS2(Karte karte) {
+        handKartenS2.remove(karte);
+    }
 
-   public void karteAblegen(Karte setKarte){
-        abgelegteKarte = setKarte;
-   }
+    public boolean darfLegen(Karte k){
+        boolean b = false;
+        if(k.getFarbe() == abgelegteKarte.getFarbe() || k.getZiffer() == abgelegteKarte.getZiffer()){
+            b = true;
+        }
+        return b;
+    }
 
 }
