069d12ef23ae3da8a1f1b226bd54ee7cd8f8df64
tryTimCatch <150668042+tryTimCatch@users.noreply.github.com>
2024-03-11 11:17:03 +0100

    Update Karte.java

--- UnoGameReal/Karte.java
+++ UnoGameReal/Karte.java
@@ -1,53 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Spieler {
-   
-    private String name;
-    public Karte abgelegteKarte;
-    private List<Karte> handKarten;
-    private List<Karte> handKartenS2;
-   
-    public Spieler(String name) {
-       this.name = name;
-       this.handKarten = new ArrayList<>();
-       this.handKartenS2 = new ArrayList<>();
-   }
-   
-   public String getName() {
-       return name;
-   }
-   
-   public List<Karte> getHandKarten() {
-       return handKarten;
-   }
-
-    public List<Karte> getHandKartenS2() {
-        return handKartenS2;
+public class Karte {     
+    private String farbe;     
+    private int ziffer;     
+    
+    public Karte(String farbe, int ziffer) {         
+        this.farbe = farbe;         
+        this.ziffer = ziffer;     
+    }     
+    
+    public String getFarbe() {         
+        return farbe;     
+    }     
+    
+    public int getZiffer() {         
+        return ziffer;     
+    }     
+        
+    @Override    
+        
+    public String toString() {         
+            return farbe + " " + ziffer;     
+        } 
     }
-   
-   public void addKarte(Karte karte) {
-       handKarten.add(karte);
-   }
-   
-    public void addKarteS2(Karte karte) {
-        handKartenS2.add(karte);
-    }
-   
-   public void removeKarte(Karte karte) {
-       handKarten.remove(karte);
-   }
-
-    public void removeKarteS2(Karte karte) {
-        handKartenS2.remove(karte);
-    }
-
-    public boolean darfLegen(Karte k){
-        boolean b = false;
-        if(k.getFarbe() == abgelegteKarte.getFarbe() || k.getZiffer() == abgelegteKarte.getZiffer()){
-            b = true;
-        }
-        return b;
-    }
-
-}
